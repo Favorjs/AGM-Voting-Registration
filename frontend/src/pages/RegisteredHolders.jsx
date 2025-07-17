f24095237183606d03249d770b9d34a7e86f5965
Apelng <administrator@apelasset.com>
2025-07-17 09:58:01 +0100

    Change contact Number

--- frontend/src/pages/RegisteredHolders.jsx
+++ frontend/src/pages/RegisteredHolders.jsx
@@ -227,7 +227,7 @@ const exportToExcel = () => {
                       <td>{user.chn || 'N/A'}</td>
                       <td>{user.email}</td>
                       <td>{user.phone_number}</td>
-                      <td>{user.holdings}</td>
+                      <td>{user.shareholding}</td>
                       <td>{formatDate(user.registered_at)}</td>
                     </tr>
                   ))
